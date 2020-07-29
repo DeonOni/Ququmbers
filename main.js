e1630245bcdecbfe495f160c02c0259a922350b6
araneoAnicet <araneoanicet@gmail.com>
2020-07-29 19:43:30 +0100

    add user router to the app

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config');
+var userRoutes = require('./routes/User');
 
 mongoose.connect(config.databaseLink);
 let db = mongoose.connection;
 var app = express();
 app.use(express.json());
+
+app.use('/user', userRoutes);
 
 app.get('/test', function(req, res) {
     res.send({result: 'Hello world!'});
