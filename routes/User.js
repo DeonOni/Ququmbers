f4016642151c6651cf1f0b83218e4a05433c9139
araneoAnicet <araneoanicet@gmail.com>
2020-07-29 18:37:03 +0100

    add user sign up route

---
+++ routes/User.js
@@ -0,0 +1,31 @@
+const express = require('express');
+let User = require('../models/User');
+let JsonResponseBuilder = require('../JsonResponseBuilder');
+
+var router = express.Router();
+router.use(express.json());
+
+// sign up route
+router.post('/user', function(req, res) {
+    var responseBuilder = new JsonResponseBuilder();
+    responseBuilder.setPayload(req);
+    User.find({email: req.body.email}, function(err, user) {
+        if (err) {  // error
+            responseBuilder.setMessage('An error occured while searching for user');
+            responseBuilder.setStatus(500);
+            return res.send(responseBuilder.build());
+        }
+        if (user) {  // user already exists
+            responseBuilder.setMessage('User with this e-mail already exists');
+            responseBuilder.setStatus(403);
+            return res.send(responseBuilder.build());
+        }
+        responseBuilder.setData({
+            name: user.name,
+            email: user.email
+        });
+        return res.send(responseBuilder.build());
+    });
+});
+
+
