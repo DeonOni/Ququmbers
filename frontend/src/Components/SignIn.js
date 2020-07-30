348a0b498ac9e297e52700a864f2f5f3b4e13d50
Epikano <epikano@ukr.net>
2020-07-30 17:50:23 +0300

    add SignIn form component

---
+++ frontend/src/Components/SignIn.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div className="formContainer">
+                <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                    Submit
+                </Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
