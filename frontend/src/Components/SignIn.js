e1cf6e2bc3410f719848cf9ca741bfc249e60675
Epikano <epikano@ukr.net>
2020-07-31 14:47:32 +0300

    add sign up/sign in system via json communication with backend server + some changes

--- frontend/src/Components/SignIn.js
+++ frontend/src/Components/SignIn.js
@@ -7,25 +7,27 @@ class SignIn extends Component {
     render() {
         return (
             <div className="formContainer">
-                <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                <Form action="http://localhost:9090/test" method="post">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="success" type="submit">
-                    Submit
-                </Button>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    
+                    <Button variant="success" type="submit">
+                        Submit
+                    </Button>
                 </Form>
             </div>
         );
--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -1,43 +1,53 @@
-import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./js/jsForSignUp";
 
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="formContainer">
+        <Form action="http://localhost:9090/test" method="post">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-class SignIn extends Component {
-    render() {
-        return (
-            <div className="formContainer">
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Username" required />
+          </Form.Group>
 
-                    <Form.Group controlId="formBasicUsername">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="password" placeholder="Username" required />
-                    </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              type="password" 
+              placeholder="Password" 
+              required 
+              id="password" 
+            />
+          </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
-                    </Form.Group>
+          <Form.Group controlId="formBasicRepeatPassword">
+            <Form.Label>Repeat password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Repeat Password"
+              required
+              id="password-check" 
+            />
+          </Form.Group>
 
-                    <Form.Group controlId="formBasicRepeatPassword">
-                        <Form.Label>Repeat password</Form.Label>
-                        <Form.Control type="password" placeholder="Repeat Password" required />
-                    </Form.Group>
-                    
-                    <Button variant="success" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        );
-    }
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default SignIn;
+export default SignUp;
