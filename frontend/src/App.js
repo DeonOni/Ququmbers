ab47441cc483b3064f599ea5d77d9eace469d90c
araneoAnicet <araneoanicet@gmail.com>
2020-07-31 20:39:36 +0100

    implement bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header.js'
+import './bootstrap.min.css'
 
 
 function App() {
