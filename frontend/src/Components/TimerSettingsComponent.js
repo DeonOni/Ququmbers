725f1c7bbf46bf4d1d6dc4277e8e5a485989a1d8
araneoAnicet <araneoanicet@gmail.com>
2020-07-31 20:37:18 +0100

    use react style

--- frontend/src/Components/TimerSettingsComponent.js
+++ frontend/src/Components/TimerSettingsComponent.js
@@ -2,44 +2,52 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-export default class TimerSettingsComponent extends Component {
-  render() {
+class TimerSettingsComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleComponent = this.toggleComponent.bind(this);
+    this.state = {
+      copmonentIsShown: false
+    }
+  }
+
+  toggleComponent() {
+    console.log('Click clock!');
+    console.log(this.state.copmonentIsShown);
+    this.setState({copmonentIsShown: !this.state.copmonentIsShown});
+  }
+
+  formRender() {
+    console.log('Form is rendered!');
     return (
       <div>
-        <div className="accordion">Timer settings</div>
-
-        <div className="panel">
-          <Form.Group>
-            <Form.Label className="settingsLabelStyle">Phase</Form.Label>
-            <Form.Control size="sm" type="text" placeholder="Large text" />
-            <Form.Label className="settingsLabelStyle">Short Break</Form.Label>
-            <Form.Control size="sm" type="text" placeholder="Large text" />
-            <Form.Label className="settingsLabelStyle">Long Break</Form.Label>
-            <Form.Control size="sm" type="text" placeholder="Large text" />
-          </Form.Group>
-
-          <Button variant="success" type="submit">
-            Submit
-          </Button>
-        </div>
+        <Form.Group>
+        <Form.Label >Phase</Form.Label>
+        <Form.Control size="sm" type="text" placeholder="Large text" />
+        <Form.Label className="settingsLabelStyle">Short Break</Form.Label>
+        <Form.Control size="sm" type="text" placeholder="Large text" />
+        <Form.Label className="settingsLabelStyle">Long Break</Form.Label>
+        <Form.Control size="sm" type="text" placeholder="Large text" />
+      </Form.Group>
+      <Button variant="success">
+      Submit
+    </Button>
       </div>
     );
   }
 
-  componentDidMount() {
-    let acc = document.querySelector(".accordion");
-    let panel = document.querySelector(".panel");
-
-    acc.addEventListener("click", () => {
-      acc.classList.toggle("active");
-
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-        acc.innerHTML = "Timer settings"
-      } else {
-        panel.style.display = "block";
-        acc.innerHTML = "Hide"
-      }
-    });
+  render() {
+    let innerJSX;
+    if (this.state.copmonentIsShown) {
+      innerJSX = this.formRender();
+    }
+    return (
+      <div>
+        <div className="accordion" onClick={this.toggleComponent}>Timer settings</div>
+          {innerJSX}
+      </div>
+    );
   }
 }
+
+export default TimerSettingsComponent;
